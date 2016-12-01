ac74ec8fd1e9cb52eca9c14e0ba6ec2d2d5dd0b0
Leo <leoproechel@gmail.com>
2016-12-01 15:52:24 -0500

    initial commit

---
+++ cypher.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+class Cypher
+
+	def initialize(sentence, shift)
+		@sentence = sentence
+		@sentence = "" if sentence == nil
+		@shift = shift
+		@output = ""
+	end
+
+	def convert(z, s)
+		z=z.ord
+		if z-s < 97
+			z=122-(s-(z-96))
+		else
+			z=z-s
+		end
+		return z.chr
+	end
+
+	def translate
+		@sentence.each_char do |x|
+			if x.match(/\w/) && x==x.upcase
+				x=x.downcase
+				s=convert(x, @shift)
+				@output << s.upcase
+			elsif x.match(/\w/)
+				@output << convert(x, @shift)
+			else
+				@output << x
+			end
+		end
+		return @output
+	end
+
+end
+
+get '/' do
+	input = params["input"]
+	cypher = Cypher.new(input, 3)
+	translated = cypher.translate
+	erb :index, :locals => {:translated => translated}
+end
